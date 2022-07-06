bcce6528c79e0627351c007f76540dadfaa2f9e4
Shyam <shyam.rayaprolu@gmail.com>
2022-07-05 21:12:20 -0400

    checking in test changes

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-# helm vars
-export HELM_RELEASE_NAME=""
+#helm cli vars
+export NAMESPACE="$BITOPS_NAMESPACE"
+export TIMEOUT="$BITOPS_TIMEOUT"
+export HELM_SET_FLAG="$BITOPS_HELM_SET_FLAG"
+export HELM_DEBUG="$BITOPS_HELM_DEBUG"
+export ATOMIC="$BITOPS_ATOMIC"
+export FORCE="$BITOPS_FORCE"
+export DRY_RUN="$BITOPS_DRY_RUN"
+
+# helm option vars
+export HELM_SKIP_DEPLOY="$BITOPS_HELM_SKIP_DEPLOY"
+export HELM_RELEASE_NAME="$BITOPS_HELM_RELEASE_NAME"
+export DEFAULT_SUB_DIR="$BITOPS_ENVROOT/$BITOPS_DEFAULT_ROOT_DIR/helm/$BITOPS_DEFAULT_SUB_DIR"
+export DEFAULT_HELM_ROOT="$BITOPS_ENVROOT/$BITOPS_DEFAULT_ROOT_DIR"
+export DEFAULT_DIR_FLAG="$BITOPS_DEFAULT_DIR_FLAG"
+export PLUGINS_ROOT_DIR="$BITOPS_PLUGINS_DIR"
+export HELM_ROOT_SCRIPTS="$BITOPS_PLUGIN_DIR"
+export HELM_ROOT_OPERATIONS="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
+export ENVIRONMENT="$BITOPS_ENVIRONMENT"
+export ENV_DIR="$BITOPS_ENVROOT"
+export ENVIRONMENT_HELM_SUBDIRECTORY="$BITOPS_ENVIRONMENT_HELM_SUBDIRECTORY"
+
+# export HELM_RELEASE_NAME=""
 export HELM_DEBUG_COMMAND=""
 export HELM_DEPLOY=${HELM_CHARTS:=false}
-export HELM_ROOT="$ENVROOT/helm" 
-export DEFAULT_HELM_ROOT="$DEFAULT_ENVROOT/helm" 
 
-# if subdirectory is not provided, iterate subdirectories
-if [ -z "$ENVIRONMENT_HELM_SUBDIRECTORY" ]; then
-  echo "ENVIRONMENT_HELM_SUBDIRECTORY not provided, iterate all helm charts in $ENV_DIR/helm"
-  for helm_chart_dir in $HELM_ROOT/*/; do
-    helm_chart_dir=${helm_chart_dir%*/}     # remove the trailing "/"
-    helm_chart_dir=${helm_chart_dir##*/}    # get everything after the final "/"
-    echo "Deploy $helm_chart_dir for $ENVIRONMENT"
-    $PLUGINS_DIR/helm/scripts/helm_handle_chart.sh $helm_chart_dir
-  done
+
+if [ -n "$HELM_SKIP_DEPLOY" ]; then
+  echo "HELM_SKIP_DEPLOY set...Skipping deployment for $ENVIRONMENT/helm/$HELM_CHART"
+  exit 0
 else
-  echo "ENVIRONMENT_HELM_SUBDIRECTORY: $ENV_DIR/helm/$ENVIRONMENT_HELM_SUBDIRECTORY"
-  $PLUGINS_DIR/helm/scripts/helm_handle_chart.sh $ENVIRONMENT_HELM_SUBDIRECTORY
+  echo "calling helm/deploy ..."
+  # if subdirectory is not provided, iterate subdirectories
+  if [ -z "$ENVIRONMENT_HELM_SUBDIRECTORY" ]; then
+    echo "ENVIRONMENT_HELM_SUBDIRECTORY not provided, iterate all helm charts in $ENV_DIR/helm"
+    for helm_chart_dir in $HELM_ROOT_OPERATIONS/*/; do
+      helm_chart_dir=${helm_chart_dir%*/}     # remove the trailing "/"
+      helm_chart_dir=${helm_chart_dir##*/}    # get everything after the final "/"
+      echo "Deploy $helm_chart_dir for $ENVIRONMENT"
+      # $HELM_ROOT_SCRIPTS/scripts/helm_handle_chart.sh $helm_chart_dir
+    done
+  else
+    echo "ENVIRONMENT_HELM_SUBDIRECTORY: $ENV_DIR/helm/$ENVIRONMENT_HELM_SUBDIRECTORY"
+    # $PLUGINS_DIR/helm/scripts/helm_handle_chart.sh $ENVIRONMENT_HELM_SUBDIRECTORY
+  fi
 fi
 
